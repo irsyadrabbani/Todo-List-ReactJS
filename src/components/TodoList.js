c39715be40c1de246cfa440da09d47e82dbed660
Irsyad Rabbani <irsyadrbbn19@gmail.com>
2022-06-22 20:29:07 +0700

    Make compleTodo function

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -14,6 +14,17 @@ function TodoList() {
 
         setTodos(newTodos);
     };
+
+    const completeTodo = id =>{
+        let updatedTodos = todos.map(todo => {
+            if(todo.id === id){
+                todo.isComplete = !todo.isComplete
+            }
+            return todo;
+        })
+        setTodos(updatedTodos);
+    };
+
   return (
     <div>
         <h1>What's the Plan for Today?</h1>
