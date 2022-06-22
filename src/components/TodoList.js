9352d5fbe95a3595e64dd457ec56f137cbe9d93b
Irsyad Rabbani <irsyadrbbn19@gmail.com>
2022-06-22 22:58:40 +0700

    Add Note

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -41,6 +41,7 @@ function TodoList() {
   return (
     <div>
         <h1>What's the Plan for Today?</h1>
+        <p>note : Click on text if you completed a task</p>
         <TodoForm onSubmit={addTodo} />
         <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo}/>
     </div>
