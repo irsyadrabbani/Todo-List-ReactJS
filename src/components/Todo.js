5f32b84ac94563baf3df776c3911f946cefd4654
Irsyad Rabbani <irsyadrbbn19@gmail.com>
2022-06-22 20:28:27 +0700

    Add onClick function in Icons

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,24 +4,28 @@ import TodoList from './TodoList'
 import { RiCloseCircleLine } from "react-icons/ri"
 import { TiEdit } from "react-icons/ti"
 
-function Todo() {
-    const [edit, SetEdit] = useState({
+function Todo({todos, completeTodo, removeTodo}) {
+    const [edit, setEdit] = useState({
         id: null,
         value: ''
-    })
+    });
 
-  return todos.map((todo, index) => {
+  return todos.map((todo, index) => (
     <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'} key={index}>
 
-        <div key={todo.id} onClick={() => complete(todo.id)}>
+        <div key={todo.id} onClick={() => completeTodo(todo.id)}>
             {todo.text}
         </div>
         <div className='icons'>
-            <RiCloseCircleLine />
-            <TiEdit />
+            <RiCloseCircleLine 
+            onClick={() => removeTodo(todo.id)}
+            className='delete-icon'
+            />
+            <TiEdit onClick={() => setEdit({id: todo.id, value: todo.text})}
+            className='edit-icon'/>
         </div>
     </div>
-  })
+  ));
 }
 
 export default Todo
